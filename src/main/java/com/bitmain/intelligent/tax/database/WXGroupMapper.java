1102aa7dabe87dc899c80f60cb6083238dda10d0
yujintao <jintao.yu@bitmain.com>
2018-06-26 12:23:02 +0800

    完成数据的关联查询

--- src/main/java/com/bitmain/intelligent/tax/database/WXGroupMapper.java
+++ src/main/java/com/bitmain/intelligent/tax/database/WXGroupMapper.java
@@ -1,5 +1,6 @@
 package com.bitmain.intelligent.tax.database;
 
+import com.bitmain.intelligent.tax.database.entity.TaxData;
 import com.bitmain.intelligent.tax.database.entity.WXGroup;
 import com.bitmain.intelligent.tax.database.entity.WXUser;
 import org.apache.ibatis.annotations.*;
@@ -19,23 +20,51 @@ public interface WXGroupMapper {
     long insert(WXGroup wxGroup);
 
 
-//    @SelectProvider(type = GroupUsersDaoProvider.class, method = "findUsersByGroupID")
-//    List<WXUser> findUsersByGroupID(Map<String,Object> params);
+
 
 
     @SelectProvider(type = GroupUsersDaoProvider.class, method = "findUsersByGroupID")
-    List<WXUser> findUsersByGroupID(Map<String,Object> params);
+    @Results(value={
+            @Result(property ="nickName",column = "nickName"),
+            @Result(property ="id",column = "id",id = true),
+            @Result(property ="gender",column = "gender"),
+            @Result(property ="avatarUrl",column = "avatarUrl"),
+            @Result(property ="city",column = "city"),
+            @Result(property ="nickName",column = "nickName"),
+            @Result(one=@One(select = "com.bitmain.intelligent.tax.database.TaxMapper.findTaxDataIfExist"),property = "taxData",column = "id",javaType = TaxData.class)
+    })
+    List<WXUser> findUsersByGroupID(Map<String, Object> params);
 
 
     @Insert("INSERT INTO wxgroup_user(group_id,user_id) select #{arg0},#{arg1} from dual where not exists (select * from wxgroup_user where  group_id=#{arg0} and  user_id=#{arg1})")
     long insertGroupUserIfNotExist(long groupID, long userID);
 
+//    做外连接查询使用
+//    @SelectProvider(type = GroupUsersDaoProvider.class, method = "findUsersByGroupID")
+//    List<WXUser> findUsersByGroupID(Map<String,Object> params);
 
 
     class GroupUsersDaoProvider {
-        public String findUsersByGroupID(Map<String,Object> params) {
+        /**
+         * 直接左外连接全部数据全都查出来。
+         * @param params
+         * @return
+         */
+//        public String findUsersByGroupID(Map<String, Object> params) {
+//            StringBuilder stringBuilder = new StringBuilder();
+//            stringBuilder.append("SELECT a.*,b.*  FROM wxuser a left join taxdata b on (b.userID = a.id) WHERE  a.id IN (select user_id from wxgroup_user where group_id=(select id from wxgroup where groupID=#{groupID}))");
+//            return stringBuilder.toString();
+//        }
+
+        /**
+         * 通过mybaits的关联查询
+         * @param params
+         * @return
+         */
+        public String findUsersByGroupID(Map<String, Object> params) {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("SELECT a.,b.*  FROM wxuser a left join taxdata b on (b.userID = a.id) WHERE  a.id IN (select user_id from wxgroup_user where group_id=(select id from wxgroup where groupID=#{groupID}))");
+            stringBuilder.append("SELECT a.nickName as nickName,a.gender as gender,a.avatarUrl as avatarUrl, " +
+                    "a.city as city,a.id as id  FROM wxuser a WHERE  a.id IN (select user_id from wxgroup_user where group_id=(select id from wxgroup where groupID=#{groupID}))");
             return stringBuilder.toString();
         }
     }
